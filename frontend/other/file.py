e0c0c57cf063db5b86100ff561a8c8b871cc254f
tynalksdfacaspiod <dlyapostuplenia@yandex.ru>
2025-06-02 20:19:13 +0300

    Added functions for board.json file

--- frontend/other/file.py
+++ frontend/other/file.py
@@ -11,3 +11,17 @@ def read_input_file():
         values = json.load(fp)
     return values
 
+
+def write_board_file(princesses_coords: set, moves_coords: set):
+    data = {
+        "princesses": princesses_coords,
+        "moves": moves_coords
+    }
+    with open("backend/data/board.json", "w") as fp:
+        json.dump(data, fp)
+
+
+def read_board_file():
+    with open("backend/data/board.json", "r") as fp:
+        data = json.load(fp)
+    return data.values()
