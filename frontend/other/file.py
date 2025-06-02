a79a70632a739e7654fa6dfa07d4c7ce3fa3748a
tynalksdfacaspiod <dlyapostuplenia@yandex.ru>
2025-06-02 18:59:28 +0300

    Added function for work with files

---
+++ frontend/other/file.py
@@ -0,0 +1,13 @@
+import json 
+import os
+
+def write_input_file(values: dict):
+    with open("backend/data/input.json", "w") as fp:
+        json.dump(values, fp)
+
+
+def read_input_file():
+    with open("backend/data/input.json", "r") as fp:
+        values = json.load(fp)
+    return values
+
