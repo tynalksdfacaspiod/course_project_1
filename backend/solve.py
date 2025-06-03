0c807d9aa41a87099f6ce9c068922029afd675ae
tynalksdfacaspiod <dlyapostuplenia@yandex.ru>
2025-06-03 13:05:59 +0300

    Fix solving algorithm

--- backend/solve.py
+++ backend/solve.py
@@ -1,65 +1,51 @@
-def get_backward_moves(princess_coords):
+def get_moves(princess_coords) -> set:
+    """ Функция возвращает ходы переданной фигуры """
     x = princess_coords[0]
     y = princess_coords[1]
 
-    moves = set()
-    for move in range(1,4):
-        # Vertical moves
-        if y-move >= 0:
-            moves.add((x,y-move))
+    def get_vertical_moves() -> set:
+        vertical_moves = set()
+        for move in range(1,4):
+            if y-move >= 0:
+                vertical_moves.add((x,y-move))
+            if y+move < N:
+                vertical_moves.add((x,y+move))
+        return vertical_moves
 
-        # Horizontal moves
-        if x-move >= 0:
-            moves.add((x-move,y))
-            
-            
-        # Diagonal moves
-        if x-move >= 0 and y-move >= 0:
-            moves.add((x-move,y-move))
-
-        if x+move < N and y-move >= 0:
-            moves.add((x+move,y-move))
-
-    return moves
-
-
-def get_forward_moves(princess_coords):
-    x = princess_coords[0]
-    y = princess_coords[1]
-
-    moves = set()
-    for move in range(1,4):
-        # Vertical moves
-        if y+move < N:
-            moves.add((x,y+move))
-
-        # Horizontal moves
-        if x+move < N:
-            moves.add((x+move,y))
-            
-            
-        # Diagonal moves
-        if x+move < N and y+move < N:
-            moves.add((x+move,y+move))
-
-        if x-move >= 0 and y+move < N:
-            moves.add((x-move,y+move))
-
-    return moves
-
-
-def get_all_moves(princess_coords):
-    all_moves = set()
-    all_moves |= get_backward_moves(princess_coords) | get_forward_moves(princess_coords)
+    def get_horizontal_moves() -> set:
+        horizontal_moves = set()
+        for move in range(1,4):
+            if x-move >= 0:
+                horizontal_moves.add((x-move,y))
+            if x+move < N:
+                horizontal_moves.add((x+move,y))
+        return horizontal_moves
     
-    return all_moves
+    def get_diagonal_moves() -> set:
+        diagonal_moves = set()
+        for move in range(1,4):
+            # Диагональные ходы "\": с левого верхнего угла к правому нижнему
+            if x-move >= 0 and y-move >= 0:
+                diagonal_moves.add((x-move,y-move))
+            if x+move < N and y+move < N:
+                diagonal_moves.add((x+move,y+move))
+
+            # Диагональные ходы "/": с левого нижнего угла к правому верхнему
+            if x-move >= 0 and y+move < N:
+                diagonal_moves.add((x-move,y+move))
+            if x+move < N and y-move >= 0:
+                diagonal_moves.add((x+move,y-move))
+        return diagonal_moves
+
+    moves = get_vertical_moves() | get_horizontal_moves() | get_diagonal_moves()
+    return moves
 
 
 def get_initial_free_squares(princesses_coords, empty_board: set()):
     occupied_squares = set()
     for princess_coords in princesses_coords:
         occupied_squares.add(princess_coords)
-        occupied_squares |= get_all_moves(princess_coords)
+        occupied_squares |= get_moves(princess_coords)
     
     return empty_board - occupied_squares
 
@@ -72,7 +58,7 @@ def init_empty_board():
     return board
 
 def get_free_squares(princess_coords, free_squares):
-    occupied_squares = get_forward_moves(princess_coords)
+    occupied_squares = get_moves(princess_coords)
     occupied_squares.add(princess_coords)
     return free_squares.copy() - occupied_squares
 
@@ -81,18 +67,27 @@ def is_result(princesses_coords):
     return len(princesses_coords) == K-1
 
 
-def solve(initial_free_squares: set, princesses_coords: set = set()):
+def solve(initial_free_squares: set, princesses_coords: set = None):
+    if princesses_coords is None:
+        princesses_coords = set()
+
     while initial_free_squares:
         root_free_square = initial_free_squares.pop()
-        princesses_coords.add(root_free_square)
+        new_princesses_coords = princesses_coords.copy()
+        new_princesses_coords.add(root_free_square)
 
 
         next_free_squares = get_free_squares(root_free_square, initial_free_squares)
-        if is_result(princesses_coords):
-            return princesses_coords, len(next_free_squares)
-        solve(next_free_squares.copy(), princesses_coords.copy())
-        
 
+        if is_result(new_princesses_coords):
+            return new_princesses_coords, next_free_squares
+
+        result = solve(next_free_squares.copy(), new_princesses_coords.copy())
+        if not result is None:
+            return result
+
+    
+    return None
 
 
 N = 8
