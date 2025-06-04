d7661318ce68bb9fcfc766a60522a935ce53175e
tynalksdfacaspiod <dlyapostuplenia@yandex.ru>
2025-06-04 23:32:24 +0300

    Added ButtonsLayout

--- frontend/layouts/render_window_layouts.py
+++ frontend/layouts/render_window_layouts.py
@@ -4,7 +4,7 @@ from PySide6.QtWidgets import (
     QPushButton
 )
 from frontend.widgets.chess_board import ChessBoard
-
+from frontend.controllers.render_window.button_controller import SaveButtonController, CloseButtonController
 
 class MainLayout(QHBoxLayout):
     def __init__(self, parent_window=None):
@@ -18,6 +18,9 @@ class MainLayout(QHBoxLayout):
         self.list_widget.setFrameShape(QFrame.Shape.StyledPanel)
         self.list_widget.itemClicked.connect(lambda item: self.parent_window.render_board(item))
 
+        self.parent_window.list_widget = self.list_widget
+        self.parent_window.board = self.chess_board
+
         self.addWidget(self.chess_board, 2)
         self.addWidget(self.list_widget, 1)
 
@@ -25,19 +28,29 @@ class MainLayout(QHBoxLayout):
         self.setContentsMargins(0,0,0,0)
 
 
+class ButtonsLayout(QHBoxLayout):
+    def __init__(self, parent_window=None):
+        super().__init__()
+        self.parent_window = parent_window
+
+        self.save_button = QPushButton("Сохранить результат")
+        self.save_button.setEnabled(False)
+        self.save_button_controller = SaveButtonController(self.parent_window)
+        self.save_button_controller.connect_to_button(self.save_button)
+
+        self.parent_window.save_button = self.save_button
+
+        self.close_button = QPushButton("Закрыть окно")
+        self.close_button_controller = CloseButtonController(self.parent_window)
+        self.close_button_controller.connect_to_button(self.close_button)
+
+        self.addWidget(self.save_button)
+        self.addWidget(self.close_button)
 
 class RenderWindowLayout(QVBoxLayout):
     def __init__(self, parent_window=None):
         super().__init__()
         self.parent_window = parent_window
 
-        layout = MainLayout(self.parent_window)
-        self.parent_window.list_widget = layout.list_widget
-        self.parent_window.board = layout.chess_board
-
-        self.save_button = QPushButton("Save data")
-        self.save_button.setEnabled(False)
-        self.parent_window.save_button = self.save_button
-        
-        self.addLayout(layout)
-        self.addWidget(self.save_button)
+        self.addLayout(MainLayout(self.parent_window))
+        self.addLayout(ButtonsLayout(self.parent_window))
