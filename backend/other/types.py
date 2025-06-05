910bbed1f6d427ebb32a16b80adbfdf0d88f448e
tynalksdfacaspiod <dlyapostuplenia@yandex.ru>
2025-06-05 14:11:22 +0300

    Added commentaries for TrackingDict

--- backend/other/types.py
+++ backend/other/types.py
@@ -1,5 +1,10 @@
+from typing import Any
+
 class TrackingDict(dict):
-    def pop(self, key, *args, **kwargs):
+    """ Словарь с дополнительной обработкой метода pop """
+
+    def pop(self, key, *args, **kwargs) -> Any:
+        """ Переопределённый метод pop для вызова дополнительной фунции """ 
         value = super().pop(key, *args, **kwargs)
         if hasattr(value, 'on_pop'):
             value.on_pop()
